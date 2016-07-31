bf6f7d40e6067e1fe016560a84fe9d54bf68019a
silentfl <silentfl@mail.ru>
2016-07-31 13:25:22 +0300

    Fix seed.rb for tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,13 @@ admin = User.find_by_email('admin@example.com')
 admin.update_attribute(:admin, true)
 admin.save!
 
+tags = Array.new(10) { Faker::Lorem.word }
+
 # create Posts
 10.times do
   admin.posts.create!(
     title: Faker::Lorem.sentence(rand(3..6)),
-    content: Faker::Lorem.paragraph(rand(30..60))
+    content: Faker::Lorem.paragraph(rand(30..60)),
+    tag_list: Array.new(rand(2..6)) { tags.sample }
   )
 end
